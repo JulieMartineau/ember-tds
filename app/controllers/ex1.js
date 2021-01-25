03b762f9a2c38064b6691b34ee6a8eaaede205a6
julie <21904615@unicaen.fr>
2021-01-25 12:12:01 +0100

    création des bouttons afficher et effacer

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -3,18 +3,27 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
-  @tracked content='';
+  @tracked content = '';
   MAX = 100;
-  get style() {
-    return
-  }
   get size() {
     return this.MAX - this.content.length;
+  }
+
+  get style() {
+    return
   }
   @tracked info;
 
   @action
   save() {
     console.log('save');
+  }
+  @action
+  clear() {
+    console.log('clear');
+  }
+  @action
+  update() {
+    console.log('update');
   }
 }
