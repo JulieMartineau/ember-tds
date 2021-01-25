92206a2932a4765eb2488eb838529a06af85a6c6
julie <21904615@unicaen.fr>
2021-01-25 11:28:35 +0100

    ajout de bootstrap

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -1,6 +1,12 @@
 import Controller from '@ember/controller';
-import {action} from '@ember/object';
-import {tracked} from "@glimmer/tracking";
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class Ex1Controller extends Controller {
+  @tracked content;
+
+  @action
+  save() {
+    console.log('save');
+  }
 }
