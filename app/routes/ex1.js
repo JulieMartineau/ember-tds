fc35629ae42856201f04e395250f5f7442c28b75
julie <21904615@unicaen.fr>
2021-02-04 18:38:29 +0100

    Fin du TD : partie 03 suite

--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
-
+import { action } from '@ember/object';
 export default class Ex1Route extends Route {
+  // modèle Hook
+  model() {
+    return { items: [] };
+  }
+
+  @action
+  save2(content) {
+    console.log('save');
+    let model = this.modelFor(this.routeName);
+    model.items = content.split('\n');
+    this.transitionTo('ex1b');
+  }
 }
