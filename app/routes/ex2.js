9257b5bb531216486795395c2483dee8ca1e6c2d
julie <21904615@unicaen.fr>
2021-02-06 23:01:06 +0100

    modification ex2.hbs, format-currency et bouton valider

--- app/routes/ex2.js
+++ app/routes/ex2.js
@@ -3,7 +3,6 @@ import { datas } from 'tds/data/datas';
 import Services from 'tds/classes/services';
 import { action, set } from '@ember/object';
 
-
 export default class Ex2Route extends Route {
   model() {
     return new Services(datas);
