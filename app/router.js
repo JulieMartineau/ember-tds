b8add9fa5a6d825dd0c74028c0bd46fc67479ced
julie <21904615@unicaen.fr>
2021-02-22 15:39:27 +0100

    erreur

--- app/router.js
+++ app/router.js
@@ -15,10 +15,10 @@ Router.map(function () {
   });
   this.route('categories', function() {
     this.route('add');
-    this.route('update',{ path: '/:category_id' });
-    this.route('contacts', { path: '/:category_id' }, function() {
+    this.route('update',{ path: '/update/:category_id' });
+    this.route('contacts', { path: '/contacts/:category_id' }, function() {
       this.route('add');
-      this.route('update',{ path: '/:category_id' });
+      this.route('update',{ path: '/contacts/update/:contact_id' });
     });
   });
 });
