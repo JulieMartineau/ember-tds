f7892b633cf172c8b99fa7b1fdc3ce46754be1a9
julie <21904615@unicaen.fr>
2021-03-12 15:01:49 +0100

    Création du service user-auth

--- app/models/employee.js
+++ app/models/employee.js
@@ -1,5 +1,7 @@
-import Model from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class EmployeeModel extends Model {
-
+  @attr name;
+  @attr email;
+  @attr password;
 }
