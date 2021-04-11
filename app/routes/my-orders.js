fb45aa4caac7400e3eadb2753acdeb0a97d676d4
julie <21904615@unicaen.fr>
2021-04-11 21:44:26 +0200

    modification route my-orders

--- app/routes/my-orders.js
+++ app/routes/my-orders.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
+import abstractroute from './abstractroute';
+import RSVP from 'rsvp';
+import { action } from '@ember/object';
 
-export default class MyOrdersRoute extends Route {
+
+export default class MyOrdersRoute extends abstractroute {
+  model() {
+    let user = this.userAuth.user;
+    if (user) {
+      return RSVP.hash({
+        orders: this.store.query('order', {
+          filter: { idEmployee: user.id },
+          include: 'orderdetail',
+        }),
+        employee: user,
+      });
+    }
+  }
 }
--- app/routes/my-orders/prepare.js
+++ app/routes/my-orders/prepare.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class MyOrdersPrepareRoute extends Route {
+  model(){
+    return {};
+  }
 }
